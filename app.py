95cafc263c35a13507a116ccfd52bc6b13958596
Josh Whiteman <joshua.whiteman98@gmail.com>
2020-07-04 18:34:30 +0100

    Created base GUI

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import time
 from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
 
 now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=200, width=400, bg="#34bccf")
+canvas.pack()
+
+root.mainloop()
