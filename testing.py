d84c1d59ea80b9f21e9adafbc3346b8bb998cade
Josh Whiteman <joshua.whiteman98@gmail.com>
2020-07-14 10:49:57 +0100

    update testing to use .after to update label

--- testing.py
+++ testing.py
@@ -1,35 +1,25 @@
-import tkinter as tk
+from tkinter import *
 import time
 from datetime import datetime
 
+
+root = Tk()
+root.title("Countdown Test")
+root.geometry("600x400")
+
+my_lable 
+
 #Countdown
-t = 0
-def countdown():
-    t = int(ent_timer.get())
+# t = 0
+# def countdown():
+#     t = int(ent_timer.get())
 
-    while t:
-        mins, secs = divmod(t, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        display_countdown.set(timeformat)
-        time.sleep(1)
-        t -= 1
-    print('SWITCH')
+#     while t:
+#         mins, secs = divmod(t, 60)
+#         timeformat = '{:02d}:{:02d}'.format(mins, secs)
+#         display_countdown.set(timeformat)
+#         time.sleep(1)
+#         t -= 1
+#     print('SWITCH')
 
-#tkinter UI design
-window = tk.Tk()
-window.title("Testing")
-
-display_countdown = tk.StringVar()
-
-frm_entry = tk.Frame(master=window)
-ent_timer = tk.Entry(master=frm_entry, width=10)
-lbl_sec = tk.Label(master=frm_entry, text="Seconds")
-
-ent_timer.grid(row=0, column=0, sticky="e")
-lbl_sec.grid(row=0, column=1, sticky="w")
-frm_entry.grid(row=0, column=0, padx=10)
-
-btn_start = tk.Button(master=window, text="START", command=countdown)
-btn_start.grid(row=0, column=1, pady=10)
-
-window.mainloop()
+root.mainloop()
